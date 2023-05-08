e0fb8a85b5872835cd203a45e0cb4bbe59a2d05d
Julia Silva <100319104+Julialost@users.noreply.github.com>
2023-05-08 02:00:51 -0300

    ok

---
+++ src/jsx/Experiencia.jsx
@@ -0,0 +1,17 @@
+import style from '../Components/Experiencia.Module.css'
+
+function Experiencia (){
+    return(
+        <section>
+            <div className="Esquerdo"></div>
+            <div className="Direito"></div>
+            <div className="Aprimorar">
+                <p>Aprimore sua experiência bancária com a nossa tecnologia.</p>
+            </div>
+
+        </section>
+
+    )
+}
+
+export default Experiencia;
---
+++ src/jsx/Header.jsx
@@ -0,0 +1,14 @@
+import style from "../Components/Header.Module.css";
+
+function Header (){
+    return(
+        <header>
+            <div className="Container">
+                <div classname="Logo">098ytfvbnm,</div>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header;
